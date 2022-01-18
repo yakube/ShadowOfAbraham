632763c6c19e69c419bcc4bdd44831dd2a5a5141
Jacob Schnoor <cubbear610@gmail.com>
2022-01-17 19:42:19 -0600

    hey

--- app/src/main/java/com/example/shadowofabraham/FirstFragment.kt
+++ app/src/main/java/com/example/shadowofabraham/FirstFragment.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.shadowofabraham.databinding.FragmentFirstBinding
-//hey
+
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
